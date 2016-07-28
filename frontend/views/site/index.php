9203fc4c462b9051c4a79d68e807041b04a5733f
Administrator <Administrator@comph>
2016-07-28 20:19:01 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,53 +1,33 @@
 <?php
 
+use yii\helpers\Html;
+use yii\helpers\Url;
 /* @var $this yii\web\View */
-
-$this->title = 'My Yii Application';
+$this->title = 'Maintenance';
 ?>
-<div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+<?php
+$route1=  Yii::$app->urlManager->createUrl('test/test1');
+?>
+<a href="<?=$route1?>"> ไปที่ Test1  </a>
+<br>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
+<?php
+$route2=  Yii::$app->urlManager->createUrl(['test/test2','name'=>'Halmeze','lname'=>'Hayi']);
+?>
+<a href="<?=$route2?>"> ไปที่ Test2  </a>
+<br>
 
-    <div class="body-content">
+<?=
+\yii\helpers\Html::a('ลิงค์แบบที่3',['test/test1']);
+?>
+<br>
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+<?=
+Html::a('ลิงค์แบบที่4',['test/test1']);
+?><br>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
-</div>
+<?=
+Url::to('ลิงค์แบบที่5',['site/index']);
+?><br>
